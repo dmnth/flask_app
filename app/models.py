ff9ad88f2d53308ce8ec44f1e7deb2821ea82a63
dmnth <kborodin1@gmail.com>
2021-11-04 23:55:52 +0200

    db

---
+++ app/models.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python3
+
+from app import db
+from datetime import datetime
+
+class Activitie(db.Model):
+
+    __tablename__ = 'activities'
+
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.Text(255), index=True, nullable=False)
+    status = db.Column(db.String(24), index=True, nullable=False)
+    date = db.Column(db.DateTime, index=True, default=datetime.utcnow())
+    
+    def __repr__(self):
+        return f"Activitie: {self.description}"
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,9 @@
 #! /usr/bin/env python3
 
 from app import app
-from flask import render_template, url_for, redirect
+from flask import render_template, url_for, redirect, session
 from app.forms import ActivitieForm
+from app.models import Activitie
 
 @app.route('/')
 @app.route('/index')
