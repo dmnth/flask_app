a7ec53a0e0fb44270e5d46da0a412913f0a96494
thdmn <kborodin1@gmail.com>
2021-11-23 21:40:02 +0200

    foldable menus

--- app/static/js/activities_long.js
+++ app/static/js/activities_long.js
@@ -1,10 +1,32 @@
 #! /usr/bin/env nodejs
 
-var element = document.querySelector('#add-stuff-top')
-var btn = document.querySelector('button.btn.btn-dark')
 
-function hideInput () {
-    element.toggleAttribute('hidden')
+var iters = document.getElementsByClassName('element left col-md-6').length
+var list_elements = document.getElementsByClassName('element left col-md-6')
+var act_btn = document.querySelector('#somebutton')
+
+function hideInputs () {
+    for (var i=0; i<iters; i++) {
+        list_elements[i].toggleAttribute('hidden')
+    }
 }
 
-btn.addEventListener('click', hideInput)
+act_btn.addEventListener('click', hideInputs)
+
+
+/*
+ *  Get button id and body id compare them in for-loop
+ *  so every button with uniq id get assigned same body with
+ *  same id
+ */
+var buttons = document.getElementsByName('button')
+
+function hide(event) {
+    idx = event.currentTarget.idx
+    list_elements[idx].toggleAttribute('hidden')
+}
+
+for (var i = 0; i<iters; i++ ) {
+    buttons[i].addEventListener('click', hide)
+    buttons[i].idx = i
+}
