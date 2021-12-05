06a06e3b7d1733897125210d83de0c3baf8c8a1e
thdmn <kborodin1@gmail.com>
2021-12-05 22:31:22 +0200

    did some frontend

--- app/static/js/jsquery_functions.js
+++ app/static/js/jsquery_functions.js
@@ -1,9 +1,45 @@
 #! /usr/bin/env nodejs
 
+$(document).ready(function() {
+    $('.slide-toggle').click(function(event) {
+        var clicked = $(this)
+        var id = clicked.attr('id')
+        $(`#${id}.box`).animate({ height: "toggle" })
+    });
+});
+
+
+
+
+
+/*
 $(document).ready(function(){
-  $(".slide-toggle").click(function(){
-    $(".box").animate({
-      width: "toggle"
+    var clicked = $(this)
+    console.log(clicked)
+    $('.slide-toggle').each(function() {
+        $(this).click( function(){
+            if ($(this) == clicked)
+            $('.box').each(function() {
+                $(this).animate({ height: "toggle"})
+            });
+        });
+    });
+});
+*/
+/*
+$(document).ready(function(){
+  $(".slide-toggle-1").click(function(){
+    $("#1").animate({
+      height: "toggle"
     });
   });
 });
+
+$(document).ready(function(){
+  $(".slide-toggle-2").click(function(){
+    $("#2").animate({
+      height: "toggle"
+    });
+  });
+});
+*/
