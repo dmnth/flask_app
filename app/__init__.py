7b2060085a44a02b5f826906a66ac64e5b76521b
thdmn <kborodin1@gmail.com>
2021-12-17 02:34:08 +0200

    errors and error logging

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,13 @@
 #! /usr/bin/env python3
 
 from flask import Flask
-from config import Config
+from config import config 
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager 
 
 app = Flask(__name__)
-app.config.from_object(Config)
+app.config.from_object(config['development'])
 login = LoginManager(app)
 # endpoint of function that handles login's
 login.login_view='login'
@@ -17,5 +17,5 @@ migrate = Migrate(app, db)
 from app.main import main as main_blueprint
 app.register_blueprint(main_blueprint)
 
-from app.main import forms, routes, models
+from app.main import forms, routes, models, errors
 
--- config.py
+++ config.py
@@ -9,12 +9,20 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or \
             'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['somemail@example.com']
+
 
 class DevelopmentConfig(Config):
     DEBUG = True
+    ENV = 'development'
     MAIL_SERVER = 'smtp.googlemail.com'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
-            'sqlite:///' + os.path.join(basedir, 'data-dev.db')
+            'sqlite:///' + os.path.join(basedir, 'app.db')
 
 class TestingConfig(Config):
     TESTING = True
