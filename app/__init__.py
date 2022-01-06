87516ac7ebe5cfa646ed87785ca8fdc05a296550
thdmn <kborodin1@gmail.com>
2022-01-06 22:01:58 +0200

    flask_mail added

--- app/__init__.py
+++ app/__init__.py
@@ -8,6 +8,7 @@ from config import config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager 
+from flask_mail import Mail
 
 
 app = Flask(__name__)
@@ -17,6 +18,7 @@ login = LoginManager(app)
 login.login_view='login'
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+mail = Mail(app)
 
 from app.main import main as main_blueprint
 app.register_blueprint(main_blueprint)
