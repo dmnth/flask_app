aceac208a7f206e2c9e5b9e9ad6d5727c5fcf2fe
thdmn <kborodin1@gmail.com>
2022-01-08 22:59:43 +0200

    added threading

--- app/main/email.py
+++ app/main/email.py
@@ -4,12 +4,16 @@ from flask import render_template
 from flask_mail import Message
 from app import mail
 from app import app
+from threading import Thread
 
+def send_async_mail(app, msg):
+    with app_context():
+        mail.send(msg)
 
 def send_mail(subject, recipients, html_body):
     msg = Message(subject=subject, recipients=recipients)
     msg.html=html_body
-    mail.send(msg)
+    Thread(target=send_async_mail, args=(app, msg)).start()
 
 def send_password_reset_email(user):
     token = user.get_reset_password_token()
